826cc7b8a0b13d0916eb9e80da496b525a3e2398
abhishekpgECE <abhishekpg10c@gmail.com>
2026-03-15 14:21:15 +0530

    Added all the codes till D2-S2

---
+++ src/assignment2/module6/SumOfElements.java
@@ -0,0 +1,13 @@
+package assignment2.module6;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+        int[] arr={4,3,5,3,2,5};
+        int sum=0;
+        for(int num:arr){
+            sum=sum+num;
+        }
+        System.out.println(sum);
+    }
+}
+
